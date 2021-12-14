ad69059e18b7990ed7a97acf1d689e0442033daa
smectaz <90684643+smectaz@users.noreply.github.com>
2021-12-14 19:04:15 +0100

    challenge-4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserProfileComponent } from "./user-profile/user-profile.component";
+import { SignupComponent } from "./signup/signup.component";
 
-const routes: Routes = [];
+const ROUTES: Routes = [
+  {
+    path: 'userProfil',
+    component: UserProfileComponent
+  },
+
+  {
+    path: 'signup',
+    component: SignupComponent
+  }
+];
+
+export { ROUTES };
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(ROUTES)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
